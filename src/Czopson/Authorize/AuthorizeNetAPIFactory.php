614f452a49ae3f636a86ff582ede14d3e17bd13d
Szeryff <artur.czopek@avalton.com>
2014-05-22 11:29:42 +0200

    Add CC Payment profile creation and clear out specs

---
+++ src/Czopson/Authorize/AuthorizeNetAPIFactory.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Artur Czopek (artur.czopek@avalton.com)
+ * Date: 5/21/14
+ * Time: 3:01 PM
+ */
+
+namespace Czopson\Authorize;
+
+
+class AuthorizeNetAPIFactory {
+
+} 
---
+++ src/Czopson/Authorize/AuthorizeNetAPIProductionFactory.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Artur Czopek (artur.czopek@avalton.com)
+ * Date: 5/21/14
+ * Time: 3:04 PM
+ */
+
+namespace Czopson\Authorize;
+
+
+class AuthorizeNetAPIProductionFactory {
+
+} 
