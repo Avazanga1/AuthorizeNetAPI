f7c56628a510d3a431bcaecfba1541ca64c08e9a
Szeryff <artur.czopek@avalton.com>
2014-05-23 12:20:08 +0200

    add removal and retrieving to Customer class

---
+++ src/Czopson/Authorize/CustomerPayment.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Artur Czopek (artur.czopek@avalton.com)
+ * Date: 5/23/14
+ * Time: 12:15 PM
+ */
+
+namespace Czopson\Authorize;
+
+
+class CustomerPayment {
+
+} 
